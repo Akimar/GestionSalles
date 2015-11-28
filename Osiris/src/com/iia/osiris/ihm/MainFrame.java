a739f5bbc9f59a850508df7f67475e867be88bcb
Massamba Sene <massamba.sene@laposte.net>
2015-11-28 23:10:34 +0100

    début ajout salarié

--- Osiris/src/com/iia/osiris/ihm/MainFrame.java
+++ Osiris/src/com/iia/osiris/ihm/MainFrame.java
@@ -345,7 +345,7 @@ public class MainFrame extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                JFrame Mf = new MainFrame();
+                MainFrame Mf = new MainFrame();
                 Mf.setLocationRelativeTo(null);
                 Mf.setVisible(true);
             }
@@ -394,6 +394,42 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void buttonAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAddActionPerformed
         // TODO add your handling code here:
+        
+        if(indexTableModel == 1)
+        {
+            /* Set the Nimbus look and feel */
+            //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AjoutSalarie.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AjoutSalarie.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AjoutSalarie.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AjoutSalarie.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+            /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                
+                    AjoutSalarie ajoutSalarie = new AjoutSalarie();
+                    ajoutSalarie.setLocationRelativeTo(null);
+                    ajoutSalarie.setVisible(true);
+                }
+            });
+         }  
     }//GEN-LAST:event_buttonAddActionPerformed
 
     private void buttonModActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonModActionPerformed

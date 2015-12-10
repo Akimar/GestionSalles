f73036e05747f16d4040acdcdefd466038a6ecef
Merlin Loison <Darkadok@gmail.com>
2015-12-10 01:11:12 +0100

    Dump final

--- SQL/gestionsalles.sql
+++ SQL/gestionsalles.sql
@@ -15,8 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---Création DB
-CREATE DATABASE gestionsalles CHARACTER SET utf8 COLLATE utf8_bin;
+-- Création DB
+CREATE DATABASE gestionsalles charset='utf8';
 use gestionsalles;
 
 --
